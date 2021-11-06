32f51d2897c8ac4ab795f5403d1d48627ebaa526
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2021-11-06 19:17:07 +0530

    Basic validation finished.

--- Login-Validation-RxSwift/Controller/LoginViewController.swift
+++ Login-Validation-RxSwift/Controller/LoginViewController.swift
@@ -6,9 +6,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class LoginViewController: UITableViewController {
 
+    struct InputValues {
+        let value: String
+        var required: Bool
+        var valid: Bool
+
+        internal init(value: String, required: Bool = true, valid: Bool = false) {
+            self.value = value
+            self.required = required
+            self.valid = valid
+        }
+    }
+    private typealias InputValidator = (_ name: String?) throws -> String
     // MARK: - Outlets
     @IBOutlet weak var firstnameTextField: UITextField!
     @IBOutlet weak var firstnameErrorLabel: UILabel!
@@ -27,13 +41,75 @@ class LoginViewController: UITableViewController {
 
     @IBOutlet weak var submitButton: UIButton!
 
+    let firstNameSubject = PublishSubject<InputValues>()
+    let lastNameSubject = PublishSubject<InputValues>()
+    let emailSubject = PublishSubject<InputValues>()
+    let countrySubject = PublishSubject<InputValues>()
+    let zipcodeSubject = PublishSubject<InputValues>()
+
+    let disposeBag = DisposeBag()
+
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configInputFields()
     }
 
-    private func configInputFields() {
+    @objc func submitButtonTapped(_ sender: UIButton) {
+//        Observable.combineLatest(firstNameSubject,
+//                                 lastNameSubject,
+//                                 emailSubject,
+//                                 countrySubject,
+//                                 zipcodeSubject)
 
+    }
+
+    private func configureSubmitButton(with validator: @escaping InputValidator) {
+        // submitButton.addTarget(self, action: #selector(submitButtonTapped), for: .touchUpInside)
+
+    }
+
+    private func configInputFields() {
+        validateInputFields(textField: firstnameTextField,
+                            errorLabel: firstnameErrorLabel,
+                            subject: firstNameSubject,
+                            validator: InputValidationService.validateFirstName)
+        validateInputFields(textField: lastnameTextField,
+                            errorLabel: lastnameErrorLabel,
+                            subject: lastNameSubject,
+                            validator: InputValidationService.validateLastName)
+        validateInputFields(textField: emailTextField,
+                            errorLabel: emailErrorLabel,
+                            subject: emailSubject,
+                            validator: InputValidationService.validateEmail)
+        validateInputFields(textField: countryTextField,
+                            errorLabel: countryErrorLabel,
+                            subject: countrySubject,
+                            validator: InputValidationService.validateCountry)
+        validateInputFields(textField: zipcodeTextField,
+                            errorLabel: zipcodeErrorLabel,
+                            subject: zipcodeSubject,
+                            validator: InputValidationService.validateZipCode)
+    }
+
+    private func validateInputFields(textField: UITextField,
+                                     errorLabel: UILabel,
+                                     subject: PublishSubject<InputValues>,
+                                     validator: @escaping InputValidator) {
+        textField.rx.text
+            .orEmpty
+            .subscribe { subject.onNext(InputValues(value: $0)) }
+            .disposed(by: disposeBag)
+
+        subject
+            .map { try validator($0.value) }
+            .catch {
+                errorLabel.isHidden = false
+                errorLabel.text = $0.localizedDescription
+                throw $0
+            }
+            .retry()
+            .subscribe { _ in errorLabel.isHidden = true }
+            .disposed(by: disposeBag)
     }
 }
--- Login-Validation-RxSwift/Global/Extension/InputValidation.swift
+++ Login-Validation-RxSwift/Global/Extension/InputValidation.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct InputValidationService {
 
-    func validateFirstName(_ name: String?) throws -> String {
+    static func validateFirstName(_ name: String?) throws -> String {
         guard let name = name else { throw ValidationError.firstNameMustBeEnter }
         guard name.count != 0 else { throw ValidationError.firstNameMustBeEnter }
         guard name.count > 1 else { throw ValidationError.firstNameTooShort }
@@ -17,7 +17,7 @@ struct InputValidationService {
         return name
     }
 
-    func validateLastName(_ name: String?) throws -> String {
+    static func validateLastName(_ name: String?) throws -> String {
         guard let name = name else { throw ValidationError.lastNameMustBeEnter }
         guard name.count != 0 else { throw ValidationError.lastNameMustBeEnter }
         guard name.count > 1 else { throw ValidationError.lastNameTooShort }
@@ -25,130 +25,33 @@ struct InputValidationService {
         return name
     }
 
-    func validateMiddleName(_ name: String?) throws -> String {
-        guard let name = name else { throw ValidationError.middleNameMustBeEnter }
-        guard name.count != 0 else { throw ValidationError.middleNameMustBeEnter }
-        guard name.count > 1 else { throw ValidationError.middleNameTooShort }
-        guard name.count < 20 else { throw ValidationError.middleNameTooLong }
-        return name
-    }
-
-    func validateGender(_ gender: String?) throws -> String {
-        guard let gender = gender else { throw ValidationError.genderMustBeSelected }
-        return gender
-    }
-
-    func validateEmail(_ email: String?) throws -> String {
+    static func validateEmail(_ email: String?) throws -> String {
         guard let email = email else { throw ValidationError.emailMustBeEnter }
         guard email.count != 0 else { throw ValidationError.emailMustBeEnter }
         guard email.isValidEmail() else { throw ValidationError.emailNotValid }
         return email
     }
 
-    func validatePassword(_ password: String?) throws -> String {
-        guard let password = password else { throw ValidationError.passwordMustBeEnter }
-        guard password.count != 0 else { throw ValidationError.passwordMustBeEnter }
-        guard password.count > 8 else { throw ValidationError.weakPassword }
-        guard password.count < 20 else { throw ValidationError.passwordTooLong }
-        guard password.isValidPassword() else { throw ValidationError.weakPassword }
-        return password
-    }
-
-    func validateConfirmPassword(_ password: String?, _ confirmPassword: String?) throws -> String {
-        guard let password = password else { throw ValidationError.confirmPasswordMustBeEnter }
-        guard password.count != 0 else { throw ValidationError.confirmPasswordMustBeEnter }
-        guard password == confirmPassword else { throw ValidationError.passwordNotSame }
-        return password
-    }
-
-    func validateIsAgeAllowed(_ date: Date?) throws -> Date {
-        guard let date = date else { throw ValidationError.ageNotAllowed }
-        guard date.validateAgeIsAllowed() else { throw ValidationError.ageNotAllowed }
-        return date
-    }
-
-    func validateAddress1(_ address: String?) throws -> String {
-        guard let address = address else { throw ValidationError.address1MustBeEnter }
-        guard address.count != 0 else { throw ValidationError.address1MustBeEnter }
-        guard address.count > 2 else { throw ValidationError.address1TooShort }
-        return address
-    }
-
-    func validateAddress2(_ address: String?) throws -> String {
-        guard let address = address else { throw ValidationError.address2MustBeEnter }
-        guard address.count != 0 else { throw ValidationError.address2MustBeEnter }
-        guard address.count > 2 else { throw ValidationError.address2TooShort }
-        return address
-    }
-
-    func validateCity(_ city: String?) throws -> String {
-        guard let city = city else { throw ValidationError.address2MustBeEnter }
-        guard city.count != 0 else { throw ValidationError.address2MustBeEnter }
-        guard city.count > 2 else { throw ValidationError.address2TooShort }
-        return city
-    }
-
-    func validateState(_ state: String?) throws -> String {
-        guard let state = state else { throw ValidationError.stateMustBeEnter }
-        guard state.count != 0 else { throw ValidationError.stateMustBeEnter }
-        guard state.count > 2 else { throw ValidationError.stateTooShort }
-        return state
-    }
-
-    func validateCountry(_ country: String?) throws -> String {
+    static func validateCountry(_ country: String?) throws -> String {
         guard let country = country else { throw ValidationError.countryMustBeEnter }
         guard country.count != 0 else { throw ValidationError.countryMustBeEnter }
         guard country.count > 2 else { throw ValidationError.countryTooShort }
         return country
     }
 
-    func validateZipCode(_ zipCode: String?) throws -> String {
-        guard let zipCode = Int(zipCode!) else { throw ValidationError.zipCodeMustBeNumeric }
-        guard zipCode > 9999 else { throw ValidationError.zipCodeMustBeEnter }
+    static func validateZipCode(_ zipCode: String?) throws -> String {
+        guard
+            let zipCodeUnwrapped = zipCode,
+            let zipCode = Int(zipCodeUnwrapped)
+        else { throw ValidationError.zipCodeMustBeNumeric }
+        guard zipCode > 1_0000 - 1 else { throw ValidationError.zipCodeMustBeEnter }
         return String(zipCode)
-    }
-
-    func validateAge(_ age: String?) throws -> Int {
-        guard let age = Int(age!) else { throw ValidationError.ageMustBeNumeric }
-        return age
-    }
-}
-
-extension Date {
-    fileprivate func validateAgeIsAllowed() -> Bool {
-        var isValid: Bool = true
-
-        // Age of 18.
-        let MINIMUM_AGE: Date = Calendar.current.date(byAdding: .year, value: -18, to: Date())!
-
-        // Age of 100.
-        let MAXIMUM_AGE: Date = Calendar.current.date(byAdding: .year, value: -100, to: Date())!
-
-        if self < MAXIMUM_AGE || self > MINIMUM_AGE {
-            isValid = false
-        }
-
-        return isValid
     }
 }
 
 extension String {
-    fileprivate func isValidPassword() -> Bool {
-        // least one uppercase,
-        // least one digit
-        // least one lowercase
-        // least one symbol
-        //  min 8 characters total
-        let password = self.trimmingCharacters(in: CharacterSet.whitespaces)
-        let passwordRegx = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&<>*~:`-]).{8,}$"
-        let passwordCheck = NSPredicate(format: "SELF MATCHES %@", passwordRegx)
-        return passwordCheck.evaluate(with: password)
-
-    }
-
     func isValidEmail() -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
         let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: self)
     }
